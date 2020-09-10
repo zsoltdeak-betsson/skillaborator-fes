ebfcc9da99360099c07a7be7afe8fb895c26328f
Zsolt Deák <lordmairtis@gmail.com>
2020-09-10 20:12:06 +0200

    feat(elaborator question): show code separately

--- src/app/component/elaborator-question.model.ts
+++ src/app/component/elaborator-question.model.ts
@@ -1,16 +1,17 @@
 export interface Question {
-    id: string;
-    value: string;
-    answers: Answer[];
-    level: number;
+  id: string;
+  value: string;
+  answers: Answer[];
+  level: number;
+  code?: string;
 }
 
 export interface Answer {
-    id: string;
-    value: string;
+  id: string;
+  value: string;
 }
 
 export interface SelectedAnswer {
-    questionId: string;
-    answerId: string;
+  questionId: string;
+  answerId: string;
 }
