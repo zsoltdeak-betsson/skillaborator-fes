fa3b20ca79db2241ae18f43357e640b099c15ef6
Zsolt Deák <lordmairtis@gmail.com>
2020-11-23 20:54:36 +0100

    feat: score chart added

--- src/app/component/elaborator-review-lobby/elaborator-review-lobby.container.component.ts
+++ src/app/component/elaborator-review-lobby/elaborator-review-lobby.container.component.ts
@@ -6,14 +6,21 @@ import {
   ChangeDetectorRef,
   ViewEncapsulation,
   OnDestroy,
+  ViewChild,
+  ElementRef,
 } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { combineLatest, Subscription } from 'rxjs';
 import { Question, SelectedAndRightAnswer } from '../elaborator-question.model';
 import { AppState } from '../../app.module';
 import { tap, filter } from 'rxjs/operators';
-import { getQuestions, getSelectedAndRightAnswers, getScore } from '../..';
+import {
+  getQuestions,
+  getSelectedAndRightAnswers,
+  getScore,
+} from '../../state';
 import { Router } from '@angular/router';
+import { Chart } from 'chart.js';
 
 enum AnswerSummaryState {
   Right,
@@ -31,6 +38,13 @@ enum AnswerSummaryState {
 export class ElaboratorReviewLobbyComponent implements OnInit, OnDestroy {
   @HostBinding('class.elaborator-review-lobby') hostCss = true;
 
+  @ViewChild('scoreChart') set scoreChart(scoreChart: ElementRef) {
+    this._scoreChart = scoreChart;
+    if (scoreChart) {
+      this.loadScoreChart();
+    }
+  }
+
   questions: Question[];
   score: number;
   selectedAndRightAnswersMap: Map<
@@ -43,6 +57,8 @@ export class ElaboratorReviewLobbyComponent implements OnInit, OnDestroy {
   answerSummaryState = AnswerSummaryState;
 
   private data$$: Subscription | undefined;
+  private _scoreChart: ElementRef | undefined;
+  private chart: Chart | undefined;
 
   constructor(
     private store: Store<AppState>,
@@ -103,6 +119,7 @@ export class ElaboratorReviewLobbyComponent implements OnInit, OnDestroy {
           this.questions = questions;
           this.score = score;
           this.isLoading = false;
+
           this.cdRef.markForCheck();
         }
       );
@@ -110,6 +127,7 @@ export class ElaboratorReviewLobbyComponent implements OnInit, OnDestroy {
 
   ngOnDestroy() {
     this.data$$?.unsubscribe();
+    this.chart?.destroy();
   }
 
   private intersect(arrA: string[], arrB: string[]): string[] {
@@ -125,5 +143,50 @@ export class ElaboratorReviewLobbyComponent implements OnInit, OnDestroy {
       intersection.push(elemA);
     });
     return intersection;
+  }
+
+  private loadScoreChart() {
+    // prettier-ignore
+    // 1/(20*sqrt(2*pi))*e^(-1/2*((x-180)/30)^2)*10^4
+    const labels = ['0', '20', '40', '60', '80', '100', '120', '140', '160', '180', '200', '220', '240', '260', '280'];
+    // prettier-ignore
+    const data: Chart.ChartData = {
+      labels,
+      datasets: [
+        {
+          data: [0, 0.00013, 0.003, 0.067, 0.77, 5.7, 27, 82, 159, 199.5, 159, 82, 27, 5.7, 0.77, 0.067],
+          radius: (context) => {
+            const index = context.dataIndex;
+            const xValue = Number(labels[index]);
+            if (Math.abs(this.score - xValue) <= 10){
+              return 4;
+            }
+            return 0;
+          }
+        },
+      ],
+    };
+    // score chart
+    const ctx = this._scoreChart.nativeElement.getContext('2d');
+    this.chart = new Chart(ctx, {
+      type: 'line',
+      data,
+      options: {
+        legend: { display: false },
+        scales: {
+          yAxes: [{ display: false }],
+          xAxes: [
+            {
+              gridLines: { drawOnChartArea: false },
+            },
+          ],
+        },
+        elements: {
+          point: { backgroundColor: 'green' },
+        },
+        tooltips: { enabled: false },
+        hover: { mode: null },
+      },
+    });
   }
 }
