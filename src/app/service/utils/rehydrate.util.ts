f9c21c22b9e19badfec2fd586bc9291cd0baec5a
Zsolt Deák <lordmairtis@gmail.com>
2021-03-07 15:11:35 +0100

    fix: update issues fix

--- src/app/service/utils/rehydrate.util.ts
+++ src/app/service/utils/rehydrate.util.ts
@@ -1,18 +1,24 @@
-import { Action, On, ActionReducer, createReducer } from '@ngrx/store';
+import {
+  Action,
+  ActionReducer,
+  createReducer,
+  ActionCreator,
+} from '@ngrx/store';
 import { StorageService, StorageType } from './storage.service';
+import { ReducerTypes } from '@ngrx/store/src/reducer_creator';
 
 export function createRehydrateReducer<S, A extends Action = Action>(
   key: string,
   initialState: S,
-  ...ons: On<S>[]
+  ...ons: ReducerTypes<S, ActionCreator[]>[]
 ): ActionReducer<S, A> {
   // rehydrate
   const newInitialState =
     StorageService.getForKey(key, StorageType.Local) ?? initialState;
 
   // new reducers save state to localstorage
-  const newOns: On<S>[] = [];
-  ons.forEach((oldOn: On<S>) => {
+  const newOns: ReducerTypes<S, ActionCreator[]>[] = [];
+  ons.forEach((oldOn: ReducerTypes<S, ActionCreator[]>) => {
     const newReducer: ActionReducer<S, A> = (
       state: S | undefined,
       action: A
