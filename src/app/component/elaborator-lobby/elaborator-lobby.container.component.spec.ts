63cb93c15d402c43100db1619899bca5f1b49e79
Zsolt <lordmairtis@gmail.com>
2021-03-07 15:13:25 +0100

    Upgrade to 11 (#5)
    * angular upgrade
    
    * material and ngrx upgrade
    
    * fix: update issues fix

--- src/app/component/elaborator-lobby/elaborator-lobby.container.component.spec.ts
+++ src/app/component/elaborator-lobby/elaborator-lobby.container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { ElaboratorLobbyComponent } from './elaborator-lobby.container.component';
 
@@ -6,7 +6,7 @@ describe('ElaboratorLobbyComponent', () => {
   let component: ElaboratorLobbyComponent;
   let fixture: ComponentFixture<ElaboratorLobbyComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ ElaboratorLobbyComponent ]
     })
